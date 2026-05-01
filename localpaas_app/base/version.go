f72240cf506b47b98cd2e07b06ab277cd620fcf4
tiendc <tiendc@gmail.com>
2026-05-01 10:03:57 +0700

    Refactor: update style of email templates

--- localpaas_app/base/version.go
+++ localpaas_app/base/version.go
@@ -18,7 +18,7 @@ const StableVersionCode = VersionCodeV1
 var StableVersion = &ReleaseInfo{
 	ReleaseDate:  timeutil.Date(time.Date(2026, time.January, 1, 0, 0, 0, 0, time.UTC)),
 	AppVersion:   "v0.1.0",
-	AppImage:     "localpaas/localpaas-dev:0.1.0",
+	AppImage:     "localpaas/localpaas-dev:v0.1.0",
 	RedisImage:   "redis:8.6-alpine",
 	DbImage:      "postgres:18.3-alpine",
 	TraefikImage: "traefik:v3.6",
@@ -30,7 +30,7 @@ const BetaVersionCode = VersionCodeV1
 var BetaVersion = &ReleaseInfo{
 	ReleaseDate:  timeutil.Date(time.Date(2026, time.January, 1, 0, 0, 0, 0, time.UTC)),
 	AppVersion:   "v0.1.0-beta1",
-	AppImage:     "localpaas/localpaas-dev:0.1.0",
+	AppImage:     "localpaas/localpaas-dev:v0.1.0",
 	RedisImage:   "redis:8.6-alpine",
 	DbImage:      "postgres:18.3-alpine",
 	TraefikImage: "traefik:v3.6",
